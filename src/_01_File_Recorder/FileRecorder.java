32b8c448f10f7051565f88317633becf2e995fc4
Chumister <alanchu4432@yahoo.com>
2021-09-18 14:04:27 -0700

    jkllk

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,19 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String stage = JOptionPane.showInputDialog("Welcome to the Metaverse");
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_recorder/FileRecorderText", true);
+			fw.write(stage + "\n");
+			fw.close();
+		}catch(Exception e) {
+			
+		}
+	}
 }
